0d136bfa9d0fcbbd598aa6cbf90630b7d8fb46df
RedPanda <thepandaro27@gmail.com>
2024-06-19 17:22:06 +0300

    v0.0.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <nav>
+        <Link className='navLink' to="/">Home</Link>
+        <Link className='navLink' to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
